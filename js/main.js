f99d46f5df95681c9f4263cef356789b6edbdc14
PxrSaN <zackquinn@outlook.es>
2023-05-18 16:55:19 -0500

    se agrega el login

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
-const images = [
-'img/background 1.jpg',
-'img/background 2.jpg',
-'img/background 3.jpg',
-'img/background 4.jpg',
 
-]   
+const images = [
+'img/imagen_1.jpg',
+'img/imagen_2.jpg',
+'img/imagen_3.jpg',
+'img/imagen_4.jpg'
+
+
+
+];
+
 
 let index = 0;
 const max = images.length;
+
 
 const containerElement = document.querySelector('.container');
 const prevSlideButton = document.querySelector('.prevSlide');
 const nextSlideButton = document.querySelector('.nextSlide');
 
-prevSlideButton.addEventListener('click', function(){
-    index--;
-    setImagenIndex();   
-    changeBackground(images[index], containerElement);
 
-});
-    
-
-nextSlideButton.addEventListener('click', function() {
-    index++;
-    setImagenIndex();
-    changeBackground(images[index], containerElement);
-
+prevSlideButton.addEventListener('click', function () {
+  index--;
+  setImageIndex();
+  changeBackgroundImage(images[index], containerElement);
 });
 
-function setImagenIndex(){
+nextSlideButton.addEventListener('click', function () {
+  index++;
+  setImageIndex();
+  changeBackgroundImage(images[index], containerElement);
+});
 
-    if(index < 0) index = max - 1;
-    if(index == max ) index = 0;
 
+function setImageIndex() {
+  if (index < 0) index = max - 1;
+  if (index === max) index = 0;
 }
 
-function changeBackground(backgroundImage, element) {
-    element.style.backgroundImage = `url(${backgroundImage})`
-
+function changeBackgroundImage(backgroundImage, element) {
+  element.style.backgroundImage = `url(${backgroundImage})`;
 }
